ff6297b5e1aecb132a6efdfe8bb3707be099b79d
Kenni Woodard <kennikristine@gmail.com>
2022-11-15 11:18:02 -0800

    book titles render upon homepage load

--- render-utils.js
+++ render-utils.js
@@ -4,4 +4,7 @@ export function renderBook(book) {
     const titleEl = document.createElement('p');
     titleEl.textContent = book.title;
     titleEl.classList.add('book-title');
+
+    bookEl.append(titleEl);
+    return bookEl;
 }
