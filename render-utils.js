d47026462b09cdbe3329786493f6ace7f7b99b7f
Kenni Woodard <kennikristine@gmail.com>
2022-11-15 10:53:29 -0800

    added html and wrote render/fetch functions

---
+++ render-utils.js
@@ -0,0 +1,7 @@
+export function renderBook(book) {
+    const bookEl = document.createElement('a');
+
+    const titleEl = document.createElement('p');
+    titleEl.textContent = book.title;
+    titleEl.classList.add('book-title');
+}
